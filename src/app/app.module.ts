f5a267a701cea7180544691d01bfa0a34339191d
liviagoncalves <liviagoncalves.morais@gmail.com>
2022-04-04 22:25:31 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {CourseListComponents} from "./courses/course-list.components";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
